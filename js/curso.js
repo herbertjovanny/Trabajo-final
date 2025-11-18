579285955613046ee5052d26ea08151db3623346
herbertjovanny <herbert.jovanny.miguel.castro@gmail.com>
2025-11-17 21:11:26 -0600

    Se agrego el codigo prporcionado por la CHAT GPT

---
+++ js/curso.js
@@ -0,0 +1,8 @@
+console.log("Página interna del curso lista.");
+
+/* Aquí puedes agregar:
+  - Guardar progreso
+  - Comentarios dinámicos
+  - Reproducción automática
+  - Botón siguiente clase
+*/
---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* Aquí puedes agregar funciones extra como:
+   - Menú móvil
+   - Guardar progreso
+   - Abrir modales de curso
+   Por ahora queda limpio
+*/
+
+console.log("Plataforma EDteam style activa.");
